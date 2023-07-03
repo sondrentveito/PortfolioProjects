e2a9fae7f021680cb4846e8d12fc5cef2785c2dd
sondrentveito <137858456+sondrentveito@users.noreply.github.com>
2023-07-03 12:08:01 +1000

    Add files via upload

---
+++ Tableau SQL Code.sql
@@ -0,0 +1,49 @@
+-- Queries used for Tableau Visualisation
+
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as signed)) as total_deaths, SUM(cast(new_deaths as signed))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDeaths
+where continent is not null 
+-- Group By date
+order by 1,2;
+
+-- Just a double check based off the data provided
+-- numbers are extremely close so we will keep them - The Second includes "International"  Location
+
+
+-- Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+-- From PortfolioProject..CovidDeaths
+-- where location = 'World'
+-- Group By date
+-- order by 1,2
+
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as signed)) as TotalDeathCount
+From CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc;
+
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From coviddeaths
+where location = "Greenland"
+Group by Location, Population
+order by PercentPopulationInfected desc;
+
+
+-- 4.
+
+Select Location, Population, date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From coviddeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc;
